341dd54d2b957211068bd318b43190eca5176b65
Will <will.lucen4@gmail.com>
2018-11-05 23:56:42 -0300

    Projeto exemplo do artigo sobre singleton no unity

---
+++ Singleton Example/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public int currentLoad;
+
+    private void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            currentLoad = 1;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        currentLoad++;
+    }
+}
---
+++ Singleton Example/Assets/Scripts/UIController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class UIController : MonoBehaviour {
+
+    public TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text.SetText(string.Format("GameController was loaded {0} times", GameController.instance.currentLoad));
+    }
+
+    public void loadScene(int sceneIndex)
+    {
+        //GameController.instance.currentLoad++;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+    }
+}
